e21b2132a164ae358cb631c5a570ffaf3e07b40e
WilliansAlb <williansalbertolpez@hotmail.com>
2019-09-10 15:40:18 -0600

    Agregando

---
+++ src/index.js
@@ -0,0 +1,23 @@
+//imports
+const express = require('express');
+const app = express();
+
+//configuracion
+app.set('view engine','ejs');
+app.set('views',__dirname+'/views');
+app.set('port',3000);
+
+//rutas
+app.get('/',(req,res)=>{
+    res.render('index',{max:15});
+});
+
+app.get('/hola',(req,res)=>{
+    res.sendFile(__dirname+'/views/saludo.html');
+});
+
+//escucha
+app.listen(app.get('port'),() =>{
+    console.log(__dirname);
+    console.log('escuchando en el puerto',app.get('port'));
+});
